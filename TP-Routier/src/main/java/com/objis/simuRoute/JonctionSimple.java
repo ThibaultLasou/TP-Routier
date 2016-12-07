2b93958e18130cf7785a5aa50a512e4bfc121ad0
tlasou <thibault.lasou@u-psud.fr>
2016-12-07 18:27:22 +0100

    Exemple de segSuivant pour Margaux

--- TP-Routier/src/main/java/com/objis/simuRoute/JonctionSimple.java
+++ TP-Routier/src/main/java/com/objis/simuRoute/JonctionSimple.java
@@ -3,9 +3,19 @@ package com.objis.simuRoute;
 public class JonctionSimple extends Jonction 
 {
 	@Override
-	Segment segSuivant(Vehicule v) 
+	Segment segSuivant(Vehicule v) throws WTFException 
 	{
-		// TODO Auto-generated method stub
-		return null;
+		for(Segment s : sesAcces)
+		{
+			if(s != v.getSaRoute())
+			{
+				return s;
+			}
+			else
+			{
+				//TODO trouver un meilleur nom d'exception
+				throw new WTFException()
+			}
+		}
 	}
 }
